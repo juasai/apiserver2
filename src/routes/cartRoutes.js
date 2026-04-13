40a34149c843ecca21b7fc651c8f9105eea5bde1
juasai <juasai@proton.me>
2026-04-12 19:43:44 -0500

    update route carrito

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -6,8 +6,9 @@ router.get('/', cartController.getAllCarts.bind(cartController));
 router.get('/:cid', cartController.getCartById.bind(cartController));
 router.post('/', cartController.createCart.bind(cartController));
 router.post('/:cid/product/:pid', cartController.addProductToCart.bind(cartController));
-router.put('/:cid/product/:pid', cartController.updateProductQuantity.bind(cartController));
-router.delete('/:cid/product/:pid', cartController.removeProductFromCart.bind(cartController));
-router.delete('/:cid', cartController.deleteCart.bind(cartController));
+router.put('/:cid', cartController.updateCart.bind(cartController));
+router.put('/:cid/products/:pid', cartController.updateProductQuantity.bind(cartController));
+router.delete('/:cid/products/:pid', cartController.removeProductFromCart.bind(cartController));
+router.delete('/:cid', cartController.clearCart.bind(cartController));
 
 module.exports = router;
